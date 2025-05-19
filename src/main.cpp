88a451aaee96f2eea21a2a5cbe25db4282903642
Mathieu Cartier <100831252+mathicarti@users.noreply.github.com>
2025-05-19 18:25:42 +1000

    Added error handling and blinking for status

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,73 @@
+#include <Arduino.h>
 #include <esp_now.h>
 #include <WiFi.h>
-#include <ESP32Servo.h>
 
-Servo myServo;
-const int SERVO_PIN = 33;
+const int POT_PIN = 13;
+const int LED_BUILTIN = 2;
+uint8_t receiverAddress[] = {0xEC, 0xC9, 0xFF, 0xE2, 0x8D, 0x18}; // MAC address of ESP32 on the car
 
-// Struct to receive data
-typedef struct {
+// Struct to send data
+typedef struct 
+{
   int angle;
-} ServoData;
+} 
+ServoData;
 
-ServoData incomingData;
+ServoData outgoingData;
 
-// Callback when data is received
-void OnDataRecv(const uint8_t * mac, const uint8_t *incomingDataBytes, int len) {
-  memcpy(&incomingData, incomingDataBytes, sizeof(incomingData));
-  Serial.print("Received angle: ");
-  Serial.println(incomingData.angle);
-  myServo.write(incomingData.angle);
+void blink (int led_PIN, int time_length)
+{
+  digitalWrite(led_PIN, HIGH);
+  delay(time_length);
+  digitalWrite(led_PIN, LOW);
+  delay(time_length);
 }
 
-void setup() {
+void setup() 
+{
   Serial.begin(115200);
+  delay(100); // Wait for terminal to init
+
+  pinMode(LED_BUILTIN, OUTPUT);
+
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
 
-  if (esp_now_init() != ESP_OK) {
+  if (esp_now_init() != ESP_OK) 
+  {
     Serial.println("Error initializing ESP-NOW");
+    for (int i = 0; i > 10; i++) // Blink 10 times if init ESP-NOW
+    {
+      blink(LED_BUILTIN, 500);
+    }
     return;
   }
 
-  esp_now_register_recv_cb(OnDataRecv);
+  esp_now_peer_info_t peerInfo = {};
+  memcpy(peerInfo.peer_addr, receiverAddress, 6);
+  peerInfo.channel = 0;
+  peerInfo.encrypt = false;
 
-  myServo.attach(SERVO_PIN);
+  if (esp_now_add_peer(&peerInfo) != ESP_OK) 
+  {
+    Serial.println("Failed to add peer");
+    for (int i = 0; i > 10; i++) // Blink 10 times if cannot connect
+    {
+      blink(LED_BUILTIN, 500);
+    }
+    return;
+  }
+    blink(LED_BUILTIN, 1000); // Blink 1 sec once if successfully connected
 }
 
-void loop() {
-  // Nothing here, everything happens via callback
+void loop() 
+{
+  int potValue = analogRead(POT_PIN);
+  int angle = map(potValue, 0, 4095, 0, 180);
+
+  outgoingData.angle = angle;
+  esp_now_send(receiverAddress, (uint8_t *)&outgoingData, sizeof(outgoingData));
+
+  Serial.printf("Sent angle: %i\n", angle);
+  delay(100);
 }
